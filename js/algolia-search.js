5c5e45a916a4a26f831bdf00351e66999dcee0dd
Pavel Asaulenko <asaulenko.pavel@gmail.com>
2021-04-07 15:07:15 +0300

    TE-8889 Added Algolia suggestions and search result page

--- js/algolia-search.js
+++ js/algolia-search.js
@@ -1,40 +1,147 @@
 const AlgoliaSearch = {
-    $searchInput: null,
-    $resultsContainer: null,
-    apiClient: null,
-    resultTemplate: null,
-    init: function (config, apiClient) {
-        this.$searchInput = $(config.searchInput);
-        this.$resultsContainer = $(config.resultsContainer);
-        this.resultTemplate = config.resultTemplate;
-        this.apiClient = apiClient;
+    pageConfig: {},
+    searchIndices: [],
+    init(searchClient, indices, pageConfig) {
+        this.searchClient = searchClient;
+        this.pageConfig = Object.assign({
+            hitsPerPage: 20,
+            container: '#tabs',
+            searchboxId: "#searchbox",
+            navigationId: "#search-navigation",
+            hitsClassName: "search-results-main",
+            statsClassName: "search-results-stats",
+            paginationClassName: "search-results-pagination"
+        }, pageConfig);
+        this.initIndices(indices, searchClient);
 
-        this.bindEventListeners();
+        return this;
     },
-    bindEventListeners: function () {
-        let keyupHandler = debounce(this.executeSearch.bind(this));
-        $(this.$searchInput).keyup(() => {
-            keyupHandler();
+    initIndices(indices, searchClient) {
+        // init extra indices
+        let extraIndices = [];
+        indices.slice(1).forEach(index => extraIndices.push(this.initSearchIndex(index, searchClient)));
+        // init main index
+        let mainIndex = this.initSearchIndex(indices[0], searchClient, helper => {
+            extraIndices.forEach(extraIndex => extraIndex.helper.setQuery(helper.state.query).search());
+            helper.search();
+        }, true);
+
+        this.searchIndices = [mainIndex].concat(extraIndices);
+    },
+    initSearchIndex(indexConfig, searchClient, searchFunction, routing) {
+        let index = instantsearch({
+            indexName: indexConfig.name,
+            searchClient: searchClient,
+            searchFunction: searchFunction,
+            searchParameters: {
+                hitsPerPage: this.pageConfig.hitsPerPage
+            },
+            routing: routing
+        });
+        index.title = indexConfig.title;
+
+        return index;
+    },
+    addSearchWidgets() {
+        this.searchIndices.forEach((searchIndex, index) => {
+            // add search box only for the main index
+            if (index === 0) {
+                searchIndex.addWidget(
+                    instantsearch.widgets.searchBox({
+                        container: this.pageConfig.searchboxId,
+                        placeholder: "Search",
+                        showSubmit: false,
+                        autofocus: true,
+                        showLoadingIndicator: false
+                    })
+                );
+            }
+
+            // hits widget
+            searchIndex.addWidget(
+                instantsearch.widgets.hits({
+                    container: $(`#tabs-${searchIndex.indexName} .${this.pageConfig.hitsClassName}`).get(0),
+                    hitsPerPage: this.pageConfig.hitsPerPage,
+                    escapeHTML: true,
+                    templates: {
+                        item: function(item) {
+                            let title = typeof item._highlightResult.title !== 'undefined'
+                                ? item._highlightResult.title.value
+                                : item.title;
+                            let content = typeof item._highlightResult.content !== 'undefined'
+                                ? item._highlightResult.content.value
+                                : '';
+                            let url = item.url
+
+                            return (
+                                '<div class="hit"><h2 class="hit-name">' +
+                                `<a href="${url}">${title}</a>` +
+                                `</h2><div class="hit-content">${content}</div></div>`
+                            );
+                        },
+                        empty:
+                            '<div class="no-results"><p>No results found.</p></div>'
+                    }
+                })
+            );
+
+            // stats widget
+            searchIndex.addWidget(
+                instantsearch.widgets.stats({
+                    container: $(`#tabs-${searchIndex.indexName} .${this.pageConfig.statsClassName}`).get(0),
+                    templates: {
+                        text: '{{#hasNoResults}}0 results{{/hasNoResults}}{{#hasOneResult}}1 result{{/hasOneResult}}{{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} results{{/hasManyResults}} found',
+                    },
+                })
+            );
+
+            // pagination widget
+            searchIndex.addWidget(
+                instantsearch.widgets.pagination({
+                    container: $(`#tabs-${searchIndex.indexName} .${this.pageConfig.paginationClassName}`).get(0),
+                    scrollTo: this.pageConfig.searchboxId
+                })
+            );
         })
     },
-    executeSearch: function () {
-        let searchTerm = this.$searchInput.val();
-        this.apiClient.search(searchTerm, this.renderResults);
+    render() {
+        let $container = $(this.pageConfig.container);
+        $container = this.addNavigation($container);
+        $container = this.addTabs($container);
+        this.addSearchWidgets();
+        this.startIndices();
+        $container.tabs();
     },
-    renderResults: function (results) {
-        AlgoliaSearch.$resultsContainer.empty();
-        results.forEach((result) => {
-            let template = AlgoliaSearch.resultTemplate.replace(/\{(\w+)\}/g, '${result.\$1}');
-            template = eval('`' + template + '`')
-            AlgoliaSearch.$resultsContainer.append($(template));
+    startIndices() {
+        // start extra-indices
+        for (let i = 1; i < this.searchIndices.length; i++) {
+            this.searchIndices[i].start();
+        }
+        // start main index
+        this.searchIndices[0].start();
+    },
+    addNavigation($container) {
+        let $navList = $(`<ul id="${this.pageConfig.navigationId}"></ul>`);
+        this.searchIndices.map(searchIndex => {
+            let $navItem = $(`<li><a href="#tabs-${searchIndex.indexName}">${searchIndex.title}</a></li>`);
+            $navList.append($navItem);
         });
-    }
-};
+        $container.append($navList);
 
-function debounce(fn, timeout = 500){
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => { fn.apply(this, args); }, timeout);
-    };
+        return $container;
+    },
+    addTabs($container) {
+        this.searchIndices.forEach(searchIndex => {
+            let $contentContainer = $(`<div id="tabs-${searchIndex.indexName}"></div>`);
+            let contentContainerHtml = `<div class="${this.pageConfig.statsClassName}"></div>` +
+                `<div class="${this.pageConfig.hitsClassName}"></div>` +
+                `<div class="${this.pageConfig.paginationClassName}"></div>`;
+
+            $contentContainer.append(contentContainerHtml);
+
+            $container.append($contentContainer);
+        })
+
+        return $container;
+    }
 }
---
+++ js/algolia-suggestions.js
@@ -0,0 +1,53 @@
+const Suggestions = {
+    searchClient: null,
+    defaultHitsPerPage: 5,
+    init(searchClient, indicesConfig, autocompleteConfig) {
+        this.searchClient = searchClient;
+        const sources = indicesConfig.map(indexConfig => this.createSource(indexConfig));
+        const {searchInput, options} = autocompleteConfig;
+        autocomplete(searchInput, options, sources);
+        this.bindSearchEvents(searchInput);
+    },
+    createSource(indexConfig) {
+        return {
+            source: autocomplete.sources.hits(
+                this.searchClient.initIndex(indexConfig.name),
+                {
+                    hitsPerPage: indexConfig.hitsPerPage || this.defaultHitsPerPage
+                }
+            ),
+            name: indexConfig.name,
+            templates: {
+                header: `<div>${indexConfig.title}</div>`,
+                suggestion(suggestion) {
+                    const title = suggestion._highlightResult.title
+                        ? suggestion._highlightResult.title.value
+                        : suggestion.title;
+                    return `<a href="${suggestion.url}">${title}</a>`;
+                },
+                empty: '<p>No matching results</p>',
+            },
+        };
+    },
+    bindSearchEvents(searchInput) {
+        $(document).keyup(function(event) {
+            if (event.which == 27) {
+                $("body").removeClass("search-active");
+            }
+        });
+
+        $(searchInput).on("keypress", function(event) {
+            let value = autocomplete.escapeHighlightedString(
+                event.target.value
+            );
+
+            if (!value) {
+                return;
+            }
+
+            if (event.which == 13) {
+                window.location = $(this).closest('form').attr('action') + `?query=${value}`;
+            }
+        });
+    }
+};

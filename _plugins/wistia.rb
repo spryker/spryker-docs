cdb83d30600bbbfcde65da4308db9bad11ea22de
skliar <skliar@zfort.com>
2021-09-13 18:07:20 +0300

    embedding wistia video

---
+++ _plugins/wistia.rb
@@ -0,0 +1,34 @@
+class Wistia < Liquid::Tag
+
+  def initialize(tagName, markup, tokens)
+    super
+    @content = markup
+    if markup =~ /^\s*([^\s]+)(?:\s+(\d+)\s+(\d+)\s*)?/i
+      @videoId = $1
+
+      if $3.nil? then
+          @width = 960
+          @height = 540
+      else
+          @width = $2.to_i
+          @height = $3.to_i
+      end
+    else
+      raise "No YouTube ID provided in the \"wistia\" tag"
+    end
+  end
+
+  def render(context)
+    w = @width
+    h = @height
+    intrinsic = ((h.to_f / w.to_f) * 100)
+    padding_top = ("%.2f" % intrinsic).to_s  + "%"
+    "<div class='post-content__video'>
+      <div class='post-content__video-wrapper' style='padding-top: #{padding_top}'>
+        <iframe  class='post-content__video-iframe' width='#{@width}' height='#{@height}' src='https://fast.wistia.net/embed/iframe/#{@videoId}' allowfullscreen></iframe>
+      </div>
+    </div>"
+  end
+
+  Liquid::Template.register_tag "wistia", self
+end

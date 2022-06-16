273fe4c52cef7d2b266d6242f27bc917ca2f0508
Pavel Asaulenko <asaulenko.pavel@gmail.com>
2022-06-16 10:30:00 +0300

    TE-11163 Removed versions

--- _plugins/canonical_url.rb
+++ _plugins/canonical_url.rb
@@ -1,39 +1,5 @@
-def get_page_versioned_urls(page)
-    full_url_pattern = %r{\A#{page.url.gsub(%r{/\d+\.\d+/}, '/\d+\.\d+/')}\Z}
-    page.site.pages.select { |site_page| site_page.url.match full_url_pattern }.map(&:url)
-end
-
-def get_page_latest_version_url(versioned_urls)
-    versioned_urls.sort_by { |version| version.scan(/(?<=\/v)\d+(?=\/)/).first.to_i }.max
-end
-
-def is_multiversion(page)
-    product = page['product']
-    role = page['role']
-
-    return false if page.site.config['versioned_categories'][product] == nil or
-        page.site.config['versioned_categories'][product][role] == nil
-
-    versioned_categories = page.site.config['versioned_categories'][product][role]
-    page.url.match(%r{\A/docs/#{product}/#{role}/(?<category>[\w-]+)/});
-    page_category = Regexp.last_match(:category)
-
-    return versioned_categories.include? page_category
-end
-
 Jekyll::Hooks.register :pages, :pre_render do |page, config|
     next page unless File.extname(page.path).match?(/md|html/)
-    next page unless is_multiversion page
 
-    versioned_urls = get_page_versioned_urls page
-
-    next page if versioned_urls.size === 1
-
-    latest_page_version_url = get_page_latest_version_url versioned_urls
-
-    next page unless latest_page_version_url != page.url
-
-    config['page']['canonical_url'] = latest_page_version_url if page.data['canonical_url'].nil? and not
-        latest_page_version_url.nil?
-    config['page']['latest_version'] = config['page']['all_versions'].first if defined?(config['page']['all_versions']) and not config['page']['all_versions'].nil?
+    config['page']['canonical_url'] = page.url
 end
--- _plugins/liquid/sidebar.rb
+++ _plugins/liquid/sidebar.rb
@@ -8,17 +8,12 @@ module Jekyll
 
     def render(context)
         @context = context
-        @version = get_current_version
         @page_url = get_page_url
         sidebar_data = get_sidebar_data
 
         return '' if sidebar_data.nil? or sidebar_data['nested'].nil?
 
         '<ul class="sidebar-nav">' + build_sidebar_string(sidebar_data['nested']) + '</ul>'
-    end
-
-    def get_current_version()
-        !@context['page']['version'].nil? ? @context['page']['version'] : @context['site']['version']
     end
 
     def get_page_url()
@@ -37,11 +32,8 @@ module Jekyll
         sidebar_string = ''
         sidebar_data.each do |nested_item|
             sidebar_item_title = nested_item['title']
-            sidebar_item_url = versionize_url(nested_item['url'])
-            include_versions = nested_item['include_versions']
+            sidebar_item_url = nested_item['url']
             sub_items = nested_item['nested']
-
-            next nested_item unless include_versions.nil? or include_versions.include? @version
 
             if sidebar_item_url.nil? then
                 sidebar_string += <<-EOF
@@ -82,28 +74,6 @@ EOF
         end
 
         return sidebar_string
-    end
-    
-    def versionize_url(url)
-        return url if url == nil or url.empty? or @version == nil or @version.empty?
-    
-        url.match(%r{\A/docs/(?<product>\w+)/(?<role>\w+)/(?<category>[\w-]+)/});
-        product = Regexp.last_match(:product)
-        role = Regexp.last_match(:role)
-        category = Regexp.last_match(:category)
-    
-        return url if not shouldBeVersioned(product, role, category)
-    
-        url_prefix = "/docs/#{product}/#{role}/#{category}/"
-        url.clone.insert(url_prefix.length, @version + "/")
-    end
-    
-    def shouldBeVersioned(product, role, category)
-        versioned_categories = @context.registers[:site].config['versioned_categories']
-    
-        versioned_categories[product] != nil and
-            versioned_categories[product][role] != nil and
-            versioned_categories[product][role].include? category
     end
   end
 end

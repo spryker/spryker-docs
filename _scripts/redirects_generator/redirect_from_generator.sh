d71987be6da57079fb0d02ef542e1d6edcc28cf8
Andrii Tserkovnyi <andrii.tserkovnyi@spryker.com>
2023-09-18 11:17:19 +0300

    Update redirect_from_generator.sh

--- _scripts/redirects_generator/redirect_from_generator.sh
+++ _scripts/redirects_generator/redirect_from_generator.sh
@@ -9,7 +9,7 @@ process_markdown_files() {
             # Check if "redirect_from:" exists in the file
             if ! grep -q "redirect_from:" "$file"; then
                 # Add "redirect_from:" after "template:" if it's missing
-                awk -v RS= -v ORS="\n\n" '/template:/ {$1=$1"\nredirect_from:"} 1' "$file" > temp && mv temp "$file"
+                awk '/template:/ && !p {print "redirect_from:"; p=1} 1' "$file" > temp && mv temp "$file"
                 
                 echo "redirect_from added to: $file"
             fi

a2b13fbf7664911392026bd8056a5c16e8ce16eb
Andrii Tserkovnyi <45360017+andriitserkovnyi@users.noreply.github.com>
2025-06-30 14:21:52 +0200

    Update page_versions.rb

--- _plugins/page_versions.rb
+++ _plugins/page_versions.rb
@@ -1,37 +1,54 @@
-# page_versions.rb
-
 def setup_current_page_version(page, config)
   return if config['page']['version']
 
-  if page.url.match(%r{/(?<version>\d+\.\d+)/})
-    config['page']['version'] = Regexp.last_match(:version)
-  end
+  page.url.match(%r{/(?<version>(\d+\.\d+|latest))/})
+  version = Regexp.last_match(:version)
+
+  config['page']['version'] = version
 end
 
 def setup_all_page_versions(page, config)
-  pattern = page.url.gsub(%r{/\d+\.\d+/}, '/\d+\\.\\d+/')
-  full_url_pattern = Regexp.new("^#{pattern}$")
+  # Normalize to numeric placeholder to detect all versions including 'latest'
+  normalized_url = page.url.gsub(%r{/(?:\d+\.\d+|latest)/}, '/\d+\\.\\d+/')
+  full_url_pattern = %r{\A#{normalized_url}\Z}
 
-  versioned_urls = page.site.pages.select { |p| p.url.match(full_url_pattern) }.map(&:url)
-  versions = extract_versions(versioned_urls)
+  versioned_page_urls = page.site.pages.select do |site_page|
+    site_page.url.match(full_url_pattern)
+  end.map(&:url)
 
-  config['page']['all_versions'] = versions.sort_by { |v| v['version'] }.reverse
+  versions = get_versions(versioned_page_urls)
+  config['page']['all_versions'] = versions.sort_by { |version| version['version'] }.reverse!
 end
 
-def extract_versions(urls)
-  version_pattern = %r{/(?<page_version>\d+\.\d+)/}
+def get_versions(versioned_page_urls)
+  version_pattern = %r{/(?<page_version>(\d+\.\d+|latest))/}
 
-  urls.map do |url|
-    if version_pattern.match(url)
-      { 'version' => Regexp.last_match(:page_version), 'url' => url }
-    end
-  end.compact
+  versioned_page_urls.map do |url|
+    version_pattern.match(url)
+    {
+      'version' => Regexp.last_match(:page_version),
+      'url' => url
+    }
+  end
+end
+
+def can_be_versioned(page)
+  product = page['product']
+  role = page['role']
+  versioned_categories = page.site.config['versioned_categories']
+
+  return false if versioned_categories[product].nil? || versioned_categories[product][role].nil?
+
+  page.url.match(%r{\A/docs/#{product}/#{role}/(?<category>[\w-]+)/})
+  page_category = Regexp.last_match(:category)
+
+  versioned_categories[product][role].include?(page_category)
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |page, config|
-  next unless File.extname(page.path).match?(/md|html/)
-  next unless page.url.start_with?('/docs/')
-  next unless page.url.match(%r{/\d+\.\d+/})  # must be a versioned path
+  next page unless File.extname(page.path).match?(/md|html/)
+  next page unless page.url.start_with?('/docs/')
+  next page unless can_be_versioned(page)
 
   setup_current_page_version(page, config)
   setup_all_page_versions(page, config)

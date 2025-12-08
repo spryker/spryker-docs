e111d7d38c4425147c76141a823545392d1b5f48
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2025-12-08 18:02:34 +0100

    improve CI to validate last_updated

--- _scripts/last_updated_checker.sh
+++ _scripts/last_updated_checker.sh
@@ -4,12 +4,16 @@ set -e
 
 BASE_SHA="${GITHUB_BASE_SHA:-HEAD^}"
 HEAD_SHA="${GITHUB_SHA:-HEAD}"
+lines_changed_limit=2
+
+echo "Comparing $BASE_SHA and $HEAD_SHA ... "
 
 files_needing_update=0
 
 changed_md_files=$(git diff --name-only "$BASE_SHA"..."$HEAD_SHA" -- | grep '\.md$' || true)
 
 if [ -z "$changed_md_files" ]; then
+  echo "No changed files were found. Maybe a wrong hash was used?"
   exit 0
 fi
 
@@ -26,10 +30,10 @@ for file in $changed_md_files; do
     continue
   fi
 
-  lines_changed=$(git diff "$BASE_SHA"..."$HEAD_SHA" -- "$file" | grep "^-.*" | grep -v "^---" | grep -v "^-$" | wc -l)
+  lines_changed=$(git diff "$BASE_SHA"..."$HEAD_SHA" -- "$file" | sed '1,/@@/d' | grep -v "^-\s*$" | grep -v "^\s*$" | wc -l)
 
-  if [ "$lines_changed" -gt 5 ]; then
-    echo "Please consider updating last_updated of the $file"
+  if [ "$lines_changed" -gt "$lines_changed_limit" ]; then
+    echo "Please consider updating last_updated of the $file with $lines_changed changed lines."
     files_needing_update=1
   fi
 done
